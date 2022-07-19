eb2f77e04533cc709ac0aad85cd4c658f431a4cc
Gavril Hasmasan <gavril.hasmasan@pitechplus.com>
2022-07-19 23:24:16 +0300

    deserialize

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\DTO\SomeDto;
 use App\Entity\Card;
 use App\Entity\User;
 use App\Repository\UserRepository;
@@ -10,20 +11,25 @@ use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Constraints\Date;
 
 class TestController extends AbstractController
 
 {
-    public function __construct(private EntityManagerInterface $em)
+    public function __construct(private EntityManagerInterface $em, private SerializerInterface $serializer)
     {
     }
 
     #[Route('/test', name: 'app_test')]
     public function index(UserRepository $userRepository): Response
     {
+
 
         $repository = $this->em->getRepository(User::class);
         $user = $userRepository->find(1);
@@ -57,9 +63,29 @@ class TestController extends AbstractController
     #[Route('/test-service', name: 'test_service')]
     public function testService(FetcherService $fetcherService): Response
     {
+
         return $this->render('test/test-service.html.twig', [
             'getUrl' => $fetcherService->get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'),
         ]);
+    }
+
+    #[Route('/serialize', name: 'serialize', methods: ["POST"])]
+    public function serializeTest(Request $request): Response
+    {
+        // Deserialize json data into Dto object
+       $someDto = $this->serializer->deserialize($request->getContent(), SomeDto::class, 'json');
+
+       dd($someDto);
+         // do something with the object
+
+        // Serialize again and send back
+
+
+//        dd($this->serializer);
+
+        return new JsonResponse([
+
+        ], 200);
     }
 
 }
---
+++ src/DTO/SomeDto.php
@@ -0,0 +1,60 @@
+<?php
+
+    namespace App\DTO;
+
+    class SomeDto implements IBaseSomeDto
+    {
+        private ?string $name;
+        private ?string $description;
+        private ?int $price;
+
+        /**
+         * @return string|null
+         */
+        public function getName(): ?string
+        {
+            return $this->name;
+        }
+
+        /**
+         * @param string|null $name
+         */
+        public function setName(?string $name): void
+        {
+            $this->name = $name;
+        }
+
+        /**
+         * @return string|null
+         */
+        public function getDescription(): ?string
+        {
+            return $this->description;
+        }
+
+        /**
+         * @param string|null $description
+         */
+        public function setDescription(?string $description): void
+        {
+            $this->description = $description;
+        }
+
+        /**
+         * @return int|null
+         */
+        public function getPrice(): ?int
+        {
+            return $this->price;
+        }
+
+        /**
+         * @param int|null $price
+         */
+        public function setPrice(?int $price): void
+        {
+            $this->price = $price;
+        }
+
+
+    }
